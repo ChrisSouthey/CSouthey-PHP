6ab27514f762fc4bbcc4fa3c2bbb3e7ee5260d80
ChrisSouthey <chsouthey@email.neit.edu>
2024-09-30 09:04:09 -0400

    Week1 stuff

---
+++ week1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <title>Chris's Site</title>
+    </head>
+
+    <body>
+
+    <h1>Hello!</h1>
+
+    <h2>Test</h2>
+
+    <h2>YEa</h2>
+    </body>
+</html>
---
+++ week1/variables.php
@@ -0,0 +1,27 @@
+<?php
+
+$Stuff = 'Apple';
+
+$first = 'Chris';
+$last = 'Southey';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <title>Chris's Site</title>
+    </head>
+
+    <body>
+
+    <h1>Vars</h1>
+
+    <h2><?= $Stuff ?></h2>
+
+    <h2><?= $first . ' ' . $last ?></h2>
+    </body>
+</html>
