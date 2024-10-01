54352036730f5cb9f58fb47cf94bd149ee1e6f69
ChrisSouthey <chsouthey@email.neit.edu>
2024-10-01 09:48:35 -0400

    C

--- week1/tasks/wk1c.php
+++ week1/tasks/wk1c.php
@@ -1,16 +1,23 @@
 <?php 
-include 'include/header/php'
+include '../includes/header.php';
+
+$animals = array('Dog', 'Cat', 'Bird', 'Horse', 'Cow', 'Chicken')
+
 
 
 ?>
 
+<h1>Week 1 Task C</h1>
+
+<ul>
+    <?php foreach($animals as $a): ?>
+        <li><?= $a; ?></li>
+    <?php endforeach; ?>
+    </ul>
 
 
 
 
 
 
-
-
-
-<?php include 'include/footer.php' ?>
+<?php include '../includes/footer.php' ?>
